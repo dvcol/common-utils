bc0f6fd4b8e68915800de71dcfd785d4483b3f31
Dinh-Van Colomban <van.colomban@gmail.com>
2024-07-22 17:26:35 +0200

    feat(window): adds isLightTheme utility function

--- lib/common/utils/window.utils.ts
+++ lib/common/utils/window.utils.ts
@@ -2,3 +2,8 @@
  * Returns true if window prefers-color-scheme is dark
  */
 export const isDarkTheme = (): boolean => window.matchMedia('(prefers-color-scheme: dark').matches;
+
+/**
+ * Returns true if window prefers-color-scheme is light
+ */
+export const isLightTheme = (): boolean => window.matchMedia('(prefers-color-scheme: light').matches;
