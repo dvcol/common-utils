e2e548231250bae6e3c7d9776c9ea2d5099758f3
Dinh-Van Colomban <van.colomban@gmail.com>
2024-08-10 01:07:40 +0200

    feat(date): add a util to get today's date in local iso

--- lib/common/utils/date.utils.ts
+++ lib/common/utils/date.utils.ts
@@ -94,3 +94,5 @@ export const dayOfTheWeek = (date: Date | string | number) => {
   const day = new Date(date).getDay();
   return NumberToDayOfWeek[day];
 };
+
+export const getTodayISOLocal = () => new Date(Date.now() - new Date().getTimezoneOffset() * 60 * 1000).toISOString();
