e4f1b776f16bee2e81e11c266e7b105bee059305
Dinh-Van Colomban <van.colomban@gmail.com>
2024-08-14 23:02:26 +0200

    feat(format): adds support to year-month-date in time formatting

--- lib/common/utils/format.utils.ts
+++ lib/common/utils/format.utils.ts
@@ -10,28 +10,73 @@ export const computeProgress = (downloaded: number | any, size: number | any): n
   return 0;
 };
 
-export type Time = { hours: number; minutes: number; seconds: number };
+export type Time = { years: number; months: number; days: number; hours: number; minutes: number; seconds: number };
 
-export const formatTimeShort = ({ hours, minutes, seconds }: Time, separator = ':'): string =>
-  [hours ? hours.toString().padStart(2, '0') : undefined, minutes.toString().padStart(2, '0'), seconds.toString().padStart(2, '0')]
-    .filter(Boolean)
-    .join(separator);
+export const formatTimeShort = ({ years, months, days, hours, minutes, seconds }: Time, separator = ':'): string => {
+  const hms = [];
+  hms.push(hours.toString().padStart(2, '0'));
+  hms.push(minutes.toString().padStart(2, '0'));
+  hms.push(seconds.toString().padStart(2, '0'));
 
-export const formatTimeLong = ({ hours, minutes, seconds }: Time): string => {
-  return [
-    hours ? `${hours.toString().padStart(2, '0')}h` : '',
-    hours ? `${minutes.toString().padStart(2, '0')}m` : `${minutes.toString().padStart(2, '0')}`,
-    `${seconds}s`,
-  ].join(' ');
+  if (!years && !months && !days) return hms.join(separator);
+
+  const ymd = [];
+  ymd.push(years.toString().padStart(2, '0'));
+  ymd.push(months.toString().padStart(2, '0'));
+  ymd.push(days.toString().padStart(2, '0'));
+
+  return `${ymd.join(separator)}-${hms.join(separator)}`;
 };
 
-export const formatTime = (s: number, format: 'short' | 'long' | 'raw' = 'short'): Time | string => {
-  const hours = Math.floor(s / (60 * 60));
-  const minutes = Math.floor(s / 60) - hours * 60;
-  const seconds = Math.floor(s) - hours * 60 * 60 - minutes * 60;
-  if (format === 'short') return formatTimeShort({ hours, minutes, seconds });
-  if (format === 'long') return formatTimeLong({ hours, minutes, seconds });
-  return { hours, minutes, seconds };
+export const formatTimeLong = ({ years, months, days, hours, minutes, seconds }: Time): string => {
+  const parts = [];
+
+  if (years) parts.push(`${years}y`);
+  if (months || years) parts.push(`${months ?? 0}m`);
+  if (days || years || months) parts.push(`${days ?? 0}d`);
+  if (hours || years || months || days) parts.push(`${hours ?? 0}h`);
+  if (minutes || years || months || days || hours) parts.push(`${minutes ?? 0}m`);
+  parts.push(`${seconds ?? 0}s`);
+
+  return parts.join(' ');
+};
+
+export const InSeconds = {
+  minute: 60,
+  hour: 60 * 60,
+  day: 60 * 60 * 24,
+  week: 60 * 60 * 24 * 7,
+  month: 60 * 60 * 24 * (365 / 12),
+  year: 60 * 60 * 24 * 365,
+} as const;
+
+export const formatHMS = (s: number): Time => {
+  const hours = Math.floor(s / InSeconds.hour);
+  const minutes = Math.floor((s % InSeconds.minute) / InSeconds.minute);
+  const seconds = s % InSeconds.minute;
+
+  return { years: 0, months: 0, days: 0, hours, minutes, seconds };
+};
+
+export const formatYMD = (s: number): Time => {
+  let remaining = 0;
+  const years = Math.floor(s / InSeconds.year);
+  remaining = s % InSeconds.year;
+
+  const months = Math.floor(remaining / InSeconds.month);
+  remaining %= InSeconds.month;
+
+  const days = Math.floor(remaining / InSeconds.day);
+  remaining %= InSeconds.day;
+
+  return { ...formatHMS(remaining), years, months, days };
+};
+
+export const formatTime = (s: number, format: 'short' | 'long' | 'ymd' | 'raw' = 'short'): Time | string => {
+  if (format === 'short') return formatTimeShort(formatHMS(s));
+  if (format === 'long') return formatTimeLong(formatHMS(s));
+  if (format === 'ymd') return formatTimeLong(formatYMD(s));
+  return formatYMD(s);
 };
 
 export const formatBytes = (byte: number | any): string => {
