c760c6e526a8f741d94254bb128f1cd08183acac
Dinh-Van Colomban <van.colomban@gmail.com>
2024-08-26 14:01:14 +0200

    feat(cache): adds additional methods to cache store

--- lib/common/models/cache.model.ts
+++ lib/common/models/cache.model.ts
@@ -7,11 +7,14 @@ export type CacheStoreEntity<V = unknown, T = string> = {
   accessedAt?: number;
 };
 
-export type CacheStore<T = unknown> = {
-  get(key: string): CacheStoreEntity<T> | Promise<CacheStoreEntity<T>> | undefined;
-  set(key: string, value: CacheStoreEntity<T>): CacheStore<T> | Promise<CacheStore<T>>;
+export type CacheStore<V = unknown, T = string> = {
+  get(key: string): CacheStoreEntity<V, T> | Promise<CacheStoreEntity<V, T>> | undefined;
+  set(key: string, value: CacheStoreEntity<V, T>): CacheStore<V, T> | Promise<CacheStore<V, T>>;
   delete(key: string): boolean | Promise<boolean>;
   clear(regex?: string): void | Promise<void>;
+  entries?(): IterableIterator<[string, CacheStoreEntity<V, T>]>;
+  values?(): IterableIterator<CacheStoreEntity<V, T>>;
+  keys?(): IterableIterator<string>;
   /** the duration in milliseconds after which the cache will be cleared */
   retention?: number;
   /** if true, the cache will be deleted if an error occurs */
