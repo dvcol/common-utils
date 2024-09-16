60bc22e94e6f95994f6f579661a365a65a19a4e7
Dinh-Van Colomban <van.colomban@gmail.com>
2024-09-16 18:33:20 +0200

    feat(promise): adds raceUntil function

---
+++ lib/common/utils/promise.utils.test.ts
@@ -0,0 +1,89 @@
+import { describe, expect, it } from 'vitest';
+
+import { raceUntil } from './promise.utils';
+
+const earlyError = new Error('Early reject');
+const lateError = new Error('Late reject');
+const trueValue = true;
+const falseValue = false;
+const truthyValue = 'truthy';
+const falsyValue = 0;
+
+describe('promise.utils.ts', () => {
+  describe('raceUntil', () => {
+    const pFalseValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(falseValue), 100);
+      });
+    const pFalsyValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(falsyValue), 300);
+      });
+    const pTrueValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(trueValue), 200);
+      });
+    const pTruthyValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(truthyValue), 400);
+      });
+    const pEarlyReject = () =>
+      new Promise((_, reject) => {
+        setTimeout(() => reject(earlyError), 100);
+      });
+    const pLateReject = () =>
+      new Promise((_, reject) => {
+        setTimeout(() => reject(lateError), 500);
+      });
+    const pNullValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(null), 100);
+      });
+    const pUndefinedValue = () =>
+      new Promise(resolve => {
+        setTimeout(() => resolve(undefined), 100);
+      });
+
+    it('should race promises until a condition returns a value', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pFalseValue(), pTrueValue(), pTruthyValue(), pFalsyValue()]);
+      await expect(outer).resolves.toBe(falseValue);
+      await expect(inner).resolves.toStrictEqual([falseValue, trueValue, truthyValue, falsyValue]);
+    });
+
+    it('should race promises until a condition returns null', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pTrueValue(), pTruthyValue(), pFalsyValue(), pNullValue(), pUndefinedValue()]);
+      await expect(outer).resolves.toBeNull();
+      await expect(inner).resolves.toStrictEqual([trueValue, truthyValue, falsyValue, null, undefined]);
+    });
+
+    it('should race promises until a condition returns a value with a custom condition', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pFalseValue(), pTrueValue(), pTruthyValue(), pFalsyValue()], r => typeof r === 'string');
+      await expect(outer).resolves.toBe(truthyValue);
+      await expect(inner).resolves.toStrictEqual([falseValue, trueValue, truthyValue, falsyValue]);
+    });
+
+    it('should race promises and resolve with null if no condition returns a value', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pFalseValue(), pFalsyValue(), pTruthyValue()], r => r === trueValue);
+      await expect(outer).resolves.toBeNull();
+      await expect(inner).resolves.toStrictEqual([falseValue, falsyValue, truthyValue]);
+    });
+
+    it('should reject if any promise rejects', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pEarlyReject(), pTrueValue(), pTruthyValue(), pLateReject()]);
+      expect(outer).rejects.toThrow(earlyError);
+      expect(inner).resolves.toBeUndefined();
+    });
+
+    it('should return outer value if inner promise resolves before outer rejects', async () => {
+      expect.assertions(2);
+      const { inner, outer } = await raceUntil([pTrueValue(), pLateReject(), pTruthyValue()]);
+      expect(outer).resolves.toBe(trueValue);
+      expect(inner).resolves.toBeUndefined();
+    });
+  });
+});
--- lib/common/utils/promise.utils.ts
+++ lib/common/utils/promise.utils.ts
@@ -2,3 +2,35 @@ export const wait = (ms?: number) =>
   new Promise(resolve => {
     setTimeout(resolve, ms);
   });
+
+/**
+ * Race promises until a one resolves with a value that satisfies a condition or if any promise rejects.
+ *
+ * @param promises - An array of promises to race
+ * @param cb - A callback that receives the resolved value and returns a boolean (default: r => r !== undefined)
+ */
+export async function raceUntil<T = unknown>(
+  promises: Promise<T>[],
+  cb = (r: T) => r !== undefined,
+): Promise<{
+  inner: Promise<void | T[]>;
+  outer: Promise<T | null>;
+}> {
+  const { promise: outer, resolve, reject } = Promise.withResolvers<T>();
+
+  const _promises = promises.map(p =>
+    p.then(result => {
+      // If an inner promise resolves with a truthy result, resolve the outer promise
+      if (cb(result)) resolve(result);
+      return result;
+    }),
+  );
+  const inner = Promise.all(_promises)
+    .catch(reject)
+    .then(result => {
+      // If no truthy result is found, resolve with null
+      resolve(null);
+      return result;
+    });
+  return { inner, outer };
+}
